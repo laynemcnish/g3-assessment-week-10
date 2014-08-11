390704a1afd753ccf790f7cd66e7d122a3504827
Layne McNish <lmcnish14@gmail.com>
2014-08-11 09:18:14 -0600

    added about page and passing tests

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -6,4 +6,11 @@ feature "Applcation" do
 
     expect(page).to have_content "Welcome"
   end
+
+  scenario "Can view about page" do
+    visit "/"
+    expect(page).to have_link "About"
+    click_link "About"
+    expect(page).to have_content "Not much to say here"
+  end
 end
